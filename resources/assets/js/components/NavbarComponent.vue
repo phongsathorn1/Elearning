6438ec210b27a4d74c416ee5da89be23bdf54a94
Phongsathron Kittiworapanya <phongsathron@outlook.com>
2017-11-13 02:50:54 +0700

    Fixed app name not show

--- resources/assets/js/components/NavbarComponent.vue
+++ resources/assets/js/components/NavbarComponent.vue
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <router-link to="/" class="navbar-brand">{{ this.$role }}</router-link>
+      <router-link to="/" class="navbar-brand">{{ this.$appName }}</router-link>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
