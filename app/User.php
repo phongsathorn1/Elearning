c01973d40c4505410d166620cac85ca2e2ae217d
Phongsathron Kittiworapanya <phongsathron@outlook.com>
2017-11-14 13:04:07 +0700

    Update model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'username', 'password',
+        'name', 'email', 'username', 'password', 'role_id'
     ];
 
     /**
