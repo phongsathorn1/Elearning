f6955af4194fd7cfca0cb2957fc3e023a45cb053
Puttimate Thammasaeng <jusmistic@gmail.com>
2017-11-17 23:58:18 +0700

    add logo file

--- public/js/app.js
+++ public/js/app.js
@@ -77425,7 +77425,7 @@ var store = new __WEBPACK_IMPORTED_MODULE_1_vuex__["a" /* default */].Store({
 /* 232 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n    margin-bottom: 0px;\n                       ^\n      Invalid CSS after \"...in-bottom: 0px;\": expected \"}\", was \"<<<<<<< HEAD\"\n      in /home/jusmistic/elearning/resources/assets/sass/design.scss (line 29, column 25)\n    at runLoaders (/home/jusmistic/elearning/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /home/jusmistic/elearning/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/jusmistic/elearning/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/jusmistic/elearning/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/jusmistic/elearning/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/home/jusmistic/elearning/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/home/jusmistic/elearning/node_modules/async/dist/async.js:958:16)\n    at options.error (/home/jusmistic/elearning/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
